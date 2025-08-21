f9f1dcf12f518e54488a0a3cb9ed38edc028d880
myavzur <shwave_id404@mail.ru>
2025-08-21 17:09:40 +0300

    + Transactions by Voice

--- app/transaction/voice.tsx
+++ app/transaction/voice.tsx
@@ -1,11 +1,63 @@
-import { Text } from "react-native";
+import {
+	AudioModule,
+	RecordingPresets,
+	setAudioModeAsync,
+	useAudioRecorder,
+	useAudioRecorderState
+} from "expo-audio";
+import { useEffect } from "react";
+import { Alert, StyleSheet } from "react-native";
 
-import { MainLayout } from "@/shared/ui";
+import { Button, MainLayout, PageHeader } from "@/shared/ui";
 
 export default function Route() {
+	const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
+	const recorderState = useAudioRecorderState(audioRecorder);
+
+	const record = async () => {
+		await audioRecorder.prepareToRecordAsync();
+		audioRecorder.record();
+	};
+
+	const stopRecording = async () => {
+		// The recording will be available on `audioRecorder.uri`.
+		await audioRecorder.stop();
+	};
+
+	useEffect(() => {
+		(async () => {
+			const status = await AudioModule.requestRecordingPermissionsAsync();
+
+			if (!status.granted) {
+				Alert.alert("Permission to access microphone was denied");
+			}
+
+			setAudioModeAsync({
+				playsInSilentMode: true,
+				allowsRecording: true
+			});
+		})();
+	}, []);
+
 	return (
 		<MainLayout>
-			<Text>Создать по голосу</Text>
+			<PageHeader title="Создать голосом" />
+
+			<Button
+				kind={recorderState.isRecording ? "outline" : "fill"}
+				onPress={recorderState.isRecording ? stopRecording : record}
+			>
+				{recorderState.isRecording ? "Stop Recording" : "Start Recording"}
+			</Button>
 		</MainLayout>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		backgroundColor: "#ecf0f1",
+		padding: 10
+	}
+});
